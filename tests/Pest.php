9f0974a195856d669d81631ffc17151abd3f416f
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-11-07 04:00:51 +0100

    :rotating_light: Fix code style

--- tests/Pest.php
+++ tests/Pest.php
@@ -26,9 +26,7 @@ declare(strict_types=1);
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+expect()->extend('toBeOne', fn () => $this->toBe(1));
 
 /*
 |--------------------------------------------------------------------------
